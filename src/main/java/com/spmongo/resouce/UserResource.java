a04e8e46f31c15983dc63884faabce6431a75c0d
lucasllf2608 <lucas.llf2608@gmail.com>
2025-06-03 08:23:41 -0300

    update user

--- src/main/java/com/spmongo/resouce/UserResource.java
+++ src/main/java/com/spmongo/resouce/UserResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,6 +69,15 @@ public class UserResource {
 		 response.put("message", "Deletado com sucesso");
 		 return ResponseEntity.ok(response); 
 		
+	}
+	
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = userService.fromDTO(objDto);
+		obj.setId(id);
+		obj = userService.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
