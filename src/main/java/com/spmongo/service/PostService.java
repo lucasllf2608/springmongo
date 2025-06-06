3c7a8f674a7690c9d3868a402d087071a82e1b9c
lucasllf2608 <lucas.llf2608@gmail.com>
2025-06-06 08:40:50 -0300

    objetendo post por id

---
+++ src/main/java/com/spmongo/service/PostService.java
@@ -0,0 +1,31 @@
+package com.spmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spmongo.domain.Post;
+import com.spmongo.repository.PostRepository;
+import com.spmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> postFind = postRepository.findById(id);
+		Post post = new Post();
+		
+		if(postFind.get() == null) {
+			throw new ObjectNotFoundException("Post não encontrado");
+		} else {
+			post = postFind.get();
+		}
+		
+		return post;
+	}
+
+}
