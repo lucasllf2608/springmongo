12608d21c65a415a566bf1c4b70d389b58d0874f
lucasllf2608 <lucas.llf2608@gmail.com>
2025-06-08 23:16:45 -0300

    consulta simples com @query

--- src/main/java/com/spmongo/service/PostService.java
+++ src/main/java/com/spmongo/service/PostService.java
@@ -31,7 +31,8 @@ public class PostService {
 	
 	
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.findByTitle(text);
 	}
+	
 
 }
